63aee12ece62844b0b17a18a8560ba1afb6dfc28
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-08-19 23:30:05 +0530

    setup navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils';
 import './global.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, 'antialiased min-h-screen pt-16')}>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { getAuthSession } from '@/lib/nextauth';
+import Link from 'next/link';
+import React from 'react';
+import SigninButton from './SigninButton';
+import UserAccountNav from './UserAccountNav';
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2 ">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+        {/* Logo */}
+        <Link href="/" className="flex items-center gap-2">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+            Quizter
+          </p>
+        </Link>
+        <div className="flex items-center">
+          {session?.user ? (
+            <UserAccountNav user={session.user} />
+          ) : (
+            <SigninButton text="Sign in" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
